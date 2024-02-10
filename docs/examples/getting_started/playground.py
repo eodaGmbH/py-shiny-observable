0acd77b6c1d9a0bf4eb82c51d47dba5ce2b7a9aa
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-02-11 00:31:54 +0100

    Update example

--- docs/examples/getting_started/playground.py
+++ docs/examples/getting_started/playground.py
@@ -6,9 +6,9 @@ from shiny import reactive
 from shiny.express import input, ui
 from shinyobservable import Observable, ObservableContext, ObservableRenderer
 
-NOTEBOOK = "https://observablehq.com/@d3/zoomable-sunburst"
+# NOTEBOOK = "https://observablehq.com/@d3/zoomable-sunburst"
 # NOTEBOOK = "https://observablehq.com/d/31ab0068a4664578"
-NOTEBOOK = "https://observablehq.com/@d3/bar-chart-race"
+# NOTEBOOK = "https://observablehq.com/@d3/bar-chart-race"
 NOTEBOOK = "https://observablehq.com/@d3/bar-chart-transitions/2"
 
 ui.h1("Observable Notebook in Shiny")
@@ -19,19 +19,21 @@ ui.hr()
 def create_data():
     return [
         dict(letter=letter, frequency=random.uniform(0, 1))
-        for letter in string.ascii_uppercase
+        for letter in string.ascii_uppercase[
+            random.randint(0, 3) : random.randint(20, 25)
+        ]
     ]
 
 
 @ObservableRenderer
 def render_notebook():
     return Observable(NOTEBOOK, cells=["viewof order", "chart", "data"]).redefine(
-        data=create_data(),
-        # data=[
-        #    dict(letter="A", frequency=0.1),
-        #    dict(letter="B", frequency=0.8),
-        #    dict(letter="C", frequency=0.6),
-        # ],
+        # data=create_data(),
+        data=[
+            dict(letter="A", frequency=0.1),
+            dict(letter="B", frequency=0.8),
+            dict(letter="C", frequency=0.6),
+        ],
     )
 
 
@@ -42,6 +44,6 @@ with ui.div(style="padding-top: 10px;"):
 @reactive.Effect
 @reactive.event(input.update_data)
 async def update_data():
-    print(input.update_data())
+    # print(input.update_data())
     async with ObservableContext("render_notebook") as nb:
         nb.redefine(data=create_data())
